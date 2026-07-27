843ac892d12963379fbb7d4f0c2542168b8b791a
3p-vln <vldknovak@gmail.com>
2026-07-27 12:44:27 +0300

    change main page (load last 2 posts), add images to bd

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+use Blog\LatestPosts;
 use Blog\PostMapper;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -27,12 +28,11 @@ try {
   exit;
 }
 
-$postMapper = new PostMapper($connection);
-
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) use ($view, $postMapper) {
-	$posts = $postMapper->getList('DESC');
+$app->get('/', function (Request $request, Response $response) use ($view, $connection) {
+	$latestPosts = new LatestPosts($connection);
+	$posts = $latestPosts->get(2);
 
   $body = $view->render("index.twig", [
 		"posts" => $posts
@@ -42,7 +42,7 @@ $app->get('/', function (Request $request, Response $response, $args) use ($view
   return $response;
 });
 
-$app->get('/about', function (Request $request, Response $response, $args) use ($view) {
+$app->get('/about', function (Request $request, Response $response) use ($view) {
   $body = $view->render("about.twig", [
           'name' => 'Vlada'
   ]);
@@ -50,8 +50,9 @@ $app->get('/about', function (Request $request, Response $response, $args) use (
   return $response;
 });
 
-$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view, $postMapper) {
-  $post = $postMapper->getByUrlKey((string) $args["url_key"]);
+$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view, $connection) {
+	$postMapper = new PostMapper($connection);
+	$post = $postMapper->getByUrlKey((string) $args["url_key"]);
 
 	if(empty($post)) {
 		$body = $view->render("not-found.twig");
